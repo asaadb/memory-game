4a6ff516eac2d1dbbf3d6c679d15bd5537f5f12d
Asaad Budayr <asad_289@hotmail.com>
2018-09-10 11:55:36 -0700

    fix: modify timer to count for min and sec only

--- js/app.js
+++ js/app.js
@@ -1,15 +1,10 @@
-/*
- * Create a list that holds all of your cards
- */
+// Create a list that holds all of your cards
 let initialCards =["fa-diamond","fa-paper-plane-o", "fa-anchor", "fa-bolt", "fa-cube", "fa-leaf", "fa-bicycle", "fa-bomb", "fa-diamond","fa-paper-plane-o", "fa-anchor", "fa-bolt", "fa-cube", "fa-leaf", "fa-bicycle", "fa-bomb"];
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
 
-// Shuffle function from http://stackoverflow.com/a/2450976
+/*
+* @description: Shuffle function from http://stackoverflow.com/a/2450976
+*/
+
 function shuffle(array) {
     var currentIndex = array.length, temporaryValue, randomIndex;
 
@@ -23,72 +18,108 @@ function shuffle(array) {
 
     return array;
 }
+
+// grap the deck cards
 const cards = document.querySelectorAll('.card');
-// suffle and update the cards
+
+/*
+* @description: suffle and update the cards
+*/
+
 function startGame(){
+
   let newCards = shuffle(initialCards);
   for (let i = 0 ; i<newCards.length; i++){
     cards[i].firstElementChild.className = `fa ${newCards[i]}`;
   }
 }
+//shuffle cards onload
 window.onload = startGame();
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
-
 
 /*
 * @description: add event listener to all the cards alowing the card to be flipped when it's cklicked. Only open two cards at the time.
 */
 
-let matches = []; // to track the number of matched cards
-let cardsClicked = []; //to track the cards that were clicked
-let moves = 0; // to track the number of moves
-let wrongGuesses = 0; //to tracke wrong guesses
+// to track the number of matched cards
+let matches = [];
+//to track the cards that were clicked
+let cardsClicked = [];
+// to track the number of moves
+let moves = 0;
+ //to tracke wrong guesses
+let wrongGuesses = 0;
 
 
+//Loop over all the cards to add evet listerners
 for (const card of cards){
 
   card.addEventListener('click', function(){
 
     //check if the card is already in the matches list
     if(!matches.includes(card.firstElementChild.classList[1])){
+      //check if the card is already open
       if (!card.classList.contains('open')){
-        cardsClicked.push(card);
-        card.classList.add('open', 'show');
-        // fisrt cklick strats the timer
         moves++;
-        if(moves === 1){
-          timing();
-        }
+        // start the timer on the first move
+        startTimer();
+        //add to the cardsClicked list
+        cardsClicked.push(card);
+        //show the card
+        card.classList.add('open', 'show');
+        //if two cards are open
         if(cardsClicked.length === 2){
-
-
           // add the move to the html
-          if(moves%2 === 0){
-          document.querySelector('.moves').textContent = moves/2;
-        }
-          setTimeout(function (){
-            for (const open of cardsClicked){
-              checkMatch(cardsClicked);
-              open.classList.remove('open', 'show');
-            }
 
-            checkWrongGuess();
-            cardsClicked = [];
-          }, 600);
+          addMove(moves);
+          //check and flip
+          cardFlip();
+        }
+      }
     }
-  }
-}});
+  });
 }
 
+/*
+* @description: start the timer on the first click
+*/
+
+function startTimer(){
+  if(moves === 1){
+    timing();
+  }
+}
+
+/*
+* @description: starts a setTimeout function to run the checkMatch and flip the cards back if no match is found
+*/
+
+function cardFlip() {
+
+  setTimeout(function (){
+
+    for (const open of cardsClicked){
+      //check if the cards match
+      checkMatch(cardsClicked);
+      //remove show and open from the two cards
+      open.classList.remove('open', 'show');
+    }
+    //check wrong matches
+    checkWrongGuess();
+    cardsClicked = [];
+  }, 600);
+}
+
+/*
+* @description: add move to the score panel.
+* @param {number} the number of moves
+*/
+
+function addMove(moves){
+  //check if two cards are flipped to count for one move
+  if(moves%2 === 0){
+    document.querySelector('.moves').textContent = moves/2;
+  }
+}
 
 /*
 * @description: Checks both cards for a match. If matched, it assigns the class 'match'
@@ -97,6 +128,7 @@ to both cards.If there is no match, classes 'open' and 'show' are removed
 */
 
 function checkMatch(allCards){
+  //get the first and second cards classes to know if the cards match
   let firstCard = allCards[0].firstElementChild.classList[1];
   let secondCard = allCards[1].firstElementChild.classList[1];
   if (!matches.includes(firstCard)){
@@ -105,17 +137,13 @@ function checkMatch(allCards){
         allCards[0].classList.add('match');
         allCards[1].classList.add('match');
         matches.push(firstCard);
-    //if the cards is not in the matches array, add it.
-
-
+        //check if matches are complete
         matchesCompleted();
+      }
+      else{
+        wrongGuesses++;
+      }
   }
-  else{
-    wrongGuesses++;
-
-  }
-}
-
 }
 
 /*
@@ -124,10 +152,10 @@ function checkMatch(allCards){
 
 function checkWrongGuess(){
 
-  if (wrongGuesses >= 8 && wrongGuesses <= 16){
+  if (wrongGuesses >= 12 && wrongGuesses <= 20){
     document.getElementById('star3').className = 'fa fa-star-o';
   }
-  if(wrongGuesses > 16){
+  if(wrongGuesses > 20){
     document.getElementById('star2').className = 'fa fa-star-o';
   }
 }
@@ -140,18 +168,16 @@ let timer = document.querySelector('.timer');
 let min = 0;
 let sec = 0;
 let hr = 0;
-//function timing(){
+let watchInterval;
+
 function timing() {
-  setInterval(function(){
-    timer.textContent = `${hr}.${min}.${sec}`;
+
+  watchInterval = setInterval(function(){
+    timer.textContent = `${min} min:${sec} sec`;
     sec++;
     if (sec >= 60){
       sec = 0;
       min++;
-    }
-    if (min >= 60){
-      min = 0;
-      hr++;
     }
   },1000);
 }
@@ -161,24 +187,30 @@ function timing() {
 */
 
 function restart(){
+
   moves = 0;
   matches = [];
   cardsClicked = [];
   wrongGuesses = 0;
+  //stop the timer
+  clearInterval(watchInterval);
+  min = 0;
+  sec = 0;
+  timer.textContent = `${min} min:${sec} sec`;
   // reset the rating
   document.getElementById('star3').className = 'fa fa-star';
   document.getElementById('star2').className = 'fa fa-star';
+  //reset the cards
   for (const num of cards){
 
     num.classList.remove('open', 'show', 'match');
     document.querySelector('.moves').textContent = moves;
 
   }
-  //new shuffle
+  //new shuffle to restart the game
   startGame();
 }
 
-//decks.addEventListener('click',timing);
 
 //add event listener to the restart button
 const restartBtn = document.querySelector('.fa-repeat');
@@ -189,9 +221,11 @@ restartBtn.addEventListener('click', restart);
 */
 
 function matchesCompleted(){
-  //console.log(matches)
+
   if(matches.length >= 8){
     setTimeout(function(){
+      //stop the timer
+      clearInterval(watchInterval);
       //get the final results
       let finalMoves = document.querySelector('.moves').textContent;
       let finalStars = document.querySelectorAll('.fa-star');
@@ -199,9 +233,9 @@ function matchesCompleted(){
       //fire the popup
       swal({
         title: "Good job! You won!",
-        text: `With ${finalMoves} moves
-        Your time: ${finalTime}
-        Rating: ${finalStars.length} stars`,
+        text: `With  ${finalMoves} moves
+        Your time:  ${finalTime}
+        Rating:  ${finalStars.length}  stars`,
         icon: "success",
         button: "Play again",
       })
